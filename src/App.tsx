138c3c16e36d15a35464bac5d4cbfdf5144d9e5f
Mark Matlock <techmatlock@gmail.com>
2024-08-24 15:50:40 -0700

    Renamed component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { NotFound } from "./pages/NotFound";
 import { Home } from "./pages/Home";
-import { Layout } from "./components/Layout";
+import { Header } from "./components/Header";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Header />}>
         <Route index element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Route>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
 
-export function Layout() {
+export function Header() {
   return <Outlet />;
 }
