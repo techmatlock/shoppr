7d8284edb229cba3f80b64d14f8baeee884067a6
Mark Matlock <techmatlock@gmail.com>
2024-08-25 14:53:19 -0700

    Added grid columns for form and sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { FaCartShopping } from "react-icons/fa6";
 export function Header() {
   return (
     <div className="grid grid-rows-[1fr_auto]">
-      <header className="flex fixed h-12 w-full outline-dashed">
+      <header className="flex h-12 outline-dashed">
         <div className="flex items-center space-x-2">
           <FaCartShopping />
           <p>shoppr</p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,16 @@ import { SideBar } from "../components/SideBar";
 
 export function Home() {
   return (
-    <main>
-      <div className="grid grid-cols-[3fr_1fr] mt-12">
-        <div>
+    <main className="grid grid-cols-1 md:grid-cols-[3fr_1fr]">
+      <section>
+        <form className="flex items-center justify-between">
+          <input type="text" className="w-full border-2 shadow-sm" placeholder="Add task" />
           <button className="p-2 rounded-md bg-purple-500">Add New</button>
-        </div>
-        <div>
-          <SideBar />
-        </div>
-      </div>
+        </form>
+      </section>
+      <section>
+        <SideBar />
+      </section>
     </main>
   );
 }
