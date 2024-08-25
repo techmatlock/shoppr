33b71fcb0adf1f39759cf0de21f8ffb9c44f4237
Mark Matlock <techmatlock@gmail.com>
2024-08-24 21:00:22 -0700

    Added tailwind styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { FaCartShopping } from "react-icons/fa6";
 
 export function Header() {
-  return <Outlet />;
+  return (
+    <div className="grid grid-rows-[1fr_auto]">
+      <header className="flex fixed h-12 w-full outline-dashed">
+        <div className="flex items-center space-x-2">
+          <FaCartShopping />
+          <p>shoppr</p>
+        </div>
+      </header>
+      <Outlet />
+    </div>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,16 @@
+import { SideBar } from "../components/SideBar";
+
 export function Home() {
-  return <p>hello</p>;
+  return (
+    <main>
+      <div className="grid grid-cols-[3fr_1fr] mt-12">
+        <div>
+          <button className="p-2 rounded-md bg-purple-500">Add New</button>
+        </div>
+        <div>
+          <SideBar />
+        </div>
+      </div>
+    </main>
+  );
 }
