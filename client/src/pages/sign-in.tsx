818482880ead9b00dce0a02ef5dc475f431972ea
Mark Matlock <techmatlock@gmail.com>
2024-08-29 10:35:47 -0700

    Fixed project structure for vite and node

--- client/src/pages/sign-in.tsx
+++ client/src/pages/sign-in.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from "@clerk/clerk-react";
 
-export default function SignInPage() {
+export function SignInPage() {
   return (
     <div className="flex justify-center my-14">
       <SignIn path="/sign-in" />
--- client/src/pages/sign-up.tsx
+++ client/src/pages/sign-up.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from "@clerk/clerk-react";
 
-export default function SignUpPage() {
+export function SignUpPage() {
   return (
     <div className="flex justify-center my-14">
       <SignUp path="/sign-up" />
