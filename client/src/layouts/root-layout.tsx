069aad75901d27ddca2adb4d34d43ae1117e4c24
Mark Matlock <techmatlock@gmail.com>
2024-08-30 10:11:00 -0700

    Removed import

--- client/src/layouts/root-layout.tsx
+++ client/src/layouts/root-layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { FaCartShopping } from "react-icons/fa6";
 
 export default function RootLayout() {
