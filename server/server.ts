09344da1e0a931c9daee0ea8da88a124735b0fdb
Mark Matlock <techmatlock@gmail.com>
2024-08-30 10:30:09 -0700

    Added express middleware

---
+++ server/server.ts
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove when used */
+import "dotenv/config";
+import express, { application } from "express";
+import pg, { Client, DatabaseError } from "pg";
+import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import { authMiddleware, ClientError, errorMiddleware } from "./lib/index.js";
+
+type User = {
+  userId: number;
+  username: string;
+  hashedPassword: string;
+};
+type Auth = {
+  username: string;
+  password: string;
+};
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const hashKey = process.env.TOKEN_SECRET;
+if (!hashKey) throw new Error("TOKEN_SECRET not found in .env");
+
+const app = express();
+
+// Create paths for static directories
+const reactStaticDir = new URL("../client/dist", import.meta.url).pathname;
+const uploadsStaticDir = new URL("public", import.meta.url).pathname;
+
+app.use(express.static(reactStaticDir));
+// Static directory for file uploads server/public/
+app.use(express.static(uploadsStaticDir));
+app.use(express.json());
+
+app.post("/api/auth/sign-up", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ClientError(400, "username and password are required fields");
+    }
+    const hashedPassword = await argon2.hash(password);
+
+    const sql = `
+      insert into "users" ("username", "hashedPassword")
+        values ($1, $2)
+        returning "userId", "username", "createdAt";
+    `;
+
+    const params = [username, hashedPassword];
+    const result = await db.query<User>(sql, params);
+    const [user] = result.rows;
+    res.status(201).json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/api/auth/sign-in", async (req, res, next) => {
+  try {
+    const { username, password } = req.body as Partial<Auth>;
+    if (!username || !password) {
+      throw new ClientError(401, "invalid login");
+    }
+
+    const sql = `
+      select "userId",
+             "hashedPassword"
+        from "users"
+        where "username" = $1
+    `;
+    const params = [username];
+    const result = await db.query(sql, params);
+    if (!result) throw new ClientError(401, `User ${username} was not found`);
+
+    const { userId, hashedPassword } = result.rows[0];
+
+    if (await argon2.verify(hashedPassword, password)) {
+      const payload = {
+        userId,
+        username,
+      };
+      const token = jwt.sign(payload, hashKey);
+      res.status(200).json({ user: payload, token });
+    } else {
+      throw new ClientError(401, "Invalid login");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+/*
+ * Handles paths that aren't handled by any other route handler.
+ * It responds with `index.html` to support page refreshes with React Router.
+ * This must be the _last_ route, just before errorMiddleware.
+ */
+app.get("*", (req, res) => res.sendFile(`${reactStaticDir}/index.html`));
+
+app.use(errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port", process.env.PORT);
+});
