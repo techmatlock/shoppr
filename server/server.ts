736b04a81d37cea466ab422912619d502c64d737
Mark Matlock <techmatlock@gmail.com>
2024-08-29 17:45:12 -0700

    Added boilerplate express

--- server/server.ts
+++ server/server.ts
@@ -1,29 +1,24 @@
 import "dotenv/config"; // To read CLERK_SECRET_KEY and CLERK_PUBLISHABLE_KEY
 import { ClerkExpressRequireAuth, RequireAuthProp, StrictAuthProp } from "@clerk/clerk-sdk-node";
-import express, { Application, Request } from "express";
+import express, { Application, Request, Response } from "express";
 
 const port = process.env.PORT || 3000;
 const app: Application = express();
 
 declare global {
-  interface Express {
-    Request: StrictAuthProp;
+  namespace Express {
+    interface Request extends StrictAuthProp {}
   }
 }
 
-// Use the strict middleware that raises an error when unauthenticated
-app.get(
-  "/protected-route",
-  ClerkExpressRequireAuth({
-    // Add options here
-    // See the Middleware options section for more details
-  }),
-  (req: RequireAuthProp<Request>, res) => {
-    res.json(req.auth);
-  }
-);
+console.log(process.env.CLERK_SECRET_KEY);
 
-app.use((err, req, res) => {
+// Use the strict middleware that raises an error when unauthenticated
+app.get("/protected-route", ClerkExpressRequireAuth({}), (req: RequireAuthProp<Request>, res) => {
+  res.json(req.auth);
+});
+
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(401).send("Unauthenticated!");
 });
