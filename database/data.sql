3434ec30e55367a7166f4739b5c15c249904ed6b
Mark Matlock <techmatlock@gmail.com>
2024-09-12 16:01:01 -0700

    Removed data.sql

--- database/data.sql
+++
@@ -1,2 +0,0 @@
-insert into "users" ("username", "hashedPassword", "createdAt")
-values ('musicfan98', '$argon2id$v=19$m=65536,t=3,p=4$drFfrBXQox0Va1L2zmxD1w$SanvtYoY0Wr3y15EAsgI8qh++81a2/K3qA6Zm8DNDro', now());
