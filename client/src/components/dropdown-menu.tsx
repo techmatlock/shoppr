ef5540ae90e9a6639705d3b5fa6a359a785f698d
Mark Matlock <techmatlock@gmail.com>
2024-09-14 13:27:02 -0700

    Added sign out link to dropdown menu

--- client/src/components/dropdown-menu.tsx
+++ client/src/components/dropdown-menu.tsx
@@ -1,9 +1,10 @@
 import { useUser } from "@/context/useUser";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export function DropDownMenu() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { user, getInitials } = useUser();
+  const { user, getInitials, handleSignOut } = useUser();
   let initials = "";
 
   if (user) {
@@ -24,30 +25,12 @@ export function DropDownMenu() {
       {isOpen && (
         <div className="absolute right-10 top-14 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
           <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-            <div>Bonnie Green</div>
-            <div className="font-medium truncate">name@flowbite.com</div>
+            <div>{user?.name}</div>
           </div>
-          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
-            <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Earnings
-              </a>
-            </li>
-          </ul>
           <div className="py-1">
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+            <Link onClick={handleSignOut} to={"/sign-in"} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
               Sign out
-            </a>
+            </Link>
           </div>
         </div>
       )}
