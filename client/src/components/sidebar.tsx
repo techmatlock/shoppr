97ea11a647a01970ba13318a3dc5c83af4b09f23
Mark Matlock <techmatlock@gmail.com>
2024-09-17 15:11:53 -0700

    Added ChatBox component

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -1,6 +1,7 @@
 import { useUser } from "@/context/useUser";
 import { getInitials } from "@/lib/data";
 import { useMutation } from "@tanstack/react-query";
+import { ChatBox } from "./chatbox";
 
 export function SideBar() {
   const { shopper, user, token, users, fetchShopper } = useUser();
@@ -45,30 +46,35 @@ export function SideBar() {
   }
 
   return (
-    <div className="flex items-center justify-center flex-col">
-      <p className="font-medium text-xl">Current ShoppR</p>
-      {existingShopper && ( // If existing shopper, let others know someone is already with user info in sidebar
-        <>
-          <div className="relative flex justify-center items-center w-10 h-10 overflow-hidden my-2 bg-green-300 rounded-full dark:bg-gray-600">
-            <span className="font-medium text-gray-600 dark:text-gray-300">{getInitials(existingShopper.name)}</span>
-          </div>
-          <div>
-            <div className="font-medium">{existingShopper.name}</div>
-          </div>
-        </>
-      )}
-      {existingShopper &&
-        isShopperLoggedIn && ( // If current logged in user matches current shopper, they have an unassign button
-          <button onClick={() => handleAction(existingShopper.userId, "remove")} className="h-10 px-2 py-2 mt-4 rounded-md text-sm font-medium whitespace-nowrap bg-red-500 hover:bg-red-700 text-slate-50 dark:bg-slate-50 dark:text-slate-900" type="submit">
-            Unassign Me
-          </button>
+    <>
+      <div className="flex items-center justify-center flex-col">
+        <p className="font-medium text-xl">Current ShoppR</p>
+        {existingShopper && ( // If existing shopper, let others know someone is already with user info in sidebar
+          <>
+            <div className="relative flex justify-center items-center w-10 h-10 overflow-hidden my-2 bg-green-300 rounded-full dark:bg-gray-600">
+              <span className="font-medium text-gray-600 dark:text-gray-300">{getInitials(existingShopper.name)}</span>
+            </div>
+            <div>
+              <div className="font-medium">{existingShopper.name}</div>
+            </div>
+          </>
         )}
-      {!existingShopper &&
-        user && ( // If current logged in user and no assigned shopper, they have an assign button
-          <button onClick={() => handleAction(user.userId, "add")} className="h-10 px-2 py-2 mt-4 rounded-md text-sm font-medium whitespace-nowrap bg-green-500 hover:bg-green-700 text-slate-50 dark:bg-slate-50 dark:text-slate-900" type="submit">
-            Assign Me
-          </button>
-        )}
-    </div>
+        {existingShopper &&
+          isShopperLoggedIn && ( // If current logged in user matches current shopper, they have an unassign button
+            <button onClick={() => handleAction(existingShopper.userId, "remove")} className="h-10 px-2 py-2 mt-4 rounded-md text-sm font-medium whitespace-nowrap bg-red-500 hover:bg-red-700 text-slate-50 dark:bg-slate-50 dark:text-slate-900" type="submit">
+              Unassign Me
+            </button>
+          )}
+        {!existingShopper &&
+          user && ( // If current logged in user and no assigned shopper, they have an assign button
+            <button onClick={() => handleAction(user.userId, "add")} className="h-10 px-2 py-2 mt-4 rounded-md text-sm font-medium whitespace-nowrap bg-green-500 hover:bg-green-700 text-slate-50 dark:bg-slate-50 dark:text-slate-900" type="submit">
+              Assign Me
+            </button>
+          )}
+      </div>
+      <div className="flex items-center mt-40">
+        <ChatBox />
+      </div>
+    </>
   );
 }
