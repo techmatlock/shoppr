7236e2c85136aad93cfd51f4a10571da9cac865e
Mark Matlock <techmatlock@gmail.com>
2024-09-18 16:14:16 -0700

    Changed list item to 5 grid columns

--- client/src/components/shopping-list.tsx
+++ client/src/components/shopping-list.tsx
@@ -45,7 +45,7 @@ export function ShoppingList({ isMobile }: Props) {
   return (
     <>
       <h1 className="text-lg font-medium my-2">On Hold</h1>
-      <div className="grid grid-cols-1 grid-flow-col md:grid-cols-4 items-center gap-8 my-6">
+      <div className="grid grid-cols-1 grid-flow-col md:grid-cols-5 items-center gap-8 my-6">
         {isMobile && (
           <>
             <p>Item</p>
@@ -57,6 +57,7 @@ export function ShoppingList({ isMobile }: Props) {
             <p>Item</p>
             <p>Status</p>
             <p>Needed By</p>
+            <p className="flex justify-center">I Need This</p>
           </>
         )}
       </div>
@@ -65,7 +66,7 @@ export function ShoppingList({ isMobile }: Props) {
           items
             ?.filter((item) => item.status === "pending")
             .map((item) => (
-              <li key={item.shoppingItemId} className="grid grid-cols-1 grid-flow-col md:grid-cols-4 items-center my-6 border-b-2">
+              <li key={item.shoppingItemId} className={`grid grid-cols-1 grid-flow-col items-center gap-4 border-b-2 py-4 ${!isMobile ? "md:grid-cols-5" : ""} ${!isMobile && existingShopper && isShopperLoggedIn ? "md:grid-cols-5" : ""}`}>
                 <div>{item.title}</div>
                 <div>
                   <span className="inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-green-400 text-white dark:bg-blue-500">{item.status}</span>
@@ -85,7 +86,7 @@ export function ShoppingList({ isMobile }: Props) {
                   </div>
                 )}
                 {!isMobile && item.userId !== user.userId && (
-                  <div className="flex items-center justify-center space-x-2">
+                  <div className="flex items-center justify-center">
                     {neededBy && neededBy.some((needed) => needed.shoppingItemId === item.shoppingItemId) && (
                       <button>
                         <FaRegTrashAlt onClick={() => removeNeededBy(user.userId, item.shoppingItemId)} className="text-2xl text-red-400" />
@@ -99,7 +100,7 @@ export function ShoppingList({ isMobile }: Props) {
                   </div>
                 )}
                 {!isMobile && existingShopper && isShopperLoggedIn && (
-                  <div className="flex justify-end">
+                  <div className="flex justify-end col-start-5">
                     <button onClick={() => handleClick(item.shoppingItemId)} className="h-10 px-2 py-2 mt-4 w-20 rounded-md text-sm font-medium whitespace-nowrap bg-red-500 hover:bg-red-700 text-slate-50 dark:bg-slate-50 dark:text-slate-900">
                       Done
                     </button>
@@ -108,18 +109,18 @@ export function ShoppingList({ isMobile }: Props) {
               </li>
             ))}
       </ul>
-      <h1 className="text-lg font-medium">Completed</h1>
+      <h1 className="text-lg font-medium mt-4">Completed</h1>
       <ul>
         {items
           ?.filter((item) => item.status === "completed")
           .map((item) => (
-            <li key={item.shoppingItemId} className="grid grid-cols-1 grid-flow-col md:grid-cols-4 items-center my-6 border-b-2 opacity-40">
+            <li key={item.shoppingItemId} className={`grid grid-cols-1 grid-flow-col items-center gap-4 border-b-2 py-4 opacity-30 ${!isMobile ? "md:grid-cols-5" : ""} ${!isMobile && existingShopper && isShopperLoggedIn ? "md:grid-cols-5" : ""}`}>
               <div>{item.title}</div>
               <div>
-                <span className="inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-red-500 text-white dark:bg-blue-500">{item.status}</span>
+                <span className="inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-red-400 text-white dark:bg-red-500">{item.status}</span>
               </div>
               {!isMobile && (
-                <div className="flex -space-x-2 rtl:space-x-reverse">
+                <div className="flex -space-x-2 rtl:space-x-reverse mx-8">
                   <div className="relative flex justify-center items-center w-10 h-10 overflow-hidden bg-blue-300 rounded-full dark:bg-gray-600">
                     <span className="font-medium text-gray-600 dark:text-gray-300">{getInitials(item.name)}</span>{" "}
                   </div>
