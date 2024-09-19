32295ec0b239cfb4b21720d4fbf01513c3ba04f9
Mark Matlock <techmatlock@gmail.com>
2024-09-18 20:29:48 -0700

    Removed cached file

--- client/src/context/useItems.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ItemsContext, ItemsContextValues } from "./ItemsContext";
-
-export function useItems(): ItemsContextValues {
-  const values = useContext(ItemsContext);
-  if (!values) throw new Error("useItems must be used inside a ItemsProvider");
-  return values;
-}
--- client/src/context/useUser.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { UserContext, UserContextValues } from "./UserContext";
-
-export function useUser(): UserContextValues {
-  const values = useContext(UserContext);
-  if (!values) throw new Error("useItems must be used inside a ItemsProvider");
-  return values;
-}
