4eb57eac2bb507b1a37a4f4f75a918fac55be32d
Mark Matlock <techmatlock@gmail.com>
2024-09-21 17:55:09 -0700

    First commit

---
+++ lambdas/authorizer/authorizer.ts
@@ -0,0 +1,39 @@
+import { APIGatewayTokenAuthorizerEvent, APIGatewayAuthorizerResult } from "aws-lambda";
+const jwt = require("jsonwebtoken");
+
+export const handler = async (event: APIGatewayTokenAuthorizerEvent) => {
+  try {
+    console.log("event", event);
+    const token = event.authorizationToken.split(" ")[1];
+
+    // Verify and decode the JWT
+    const payload = jwt.verify(token, process.env.SECRET_KEY, {
+      algorithms: ["HS256"],
+    });
+
+    const principalId: string = payload.sub || "user";
+
+    return generatePolicy(principalId, "Allow", event.methodArn);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Token validation failed:", error.message);
+      throw new Error("Unauthorized");
+    }
+  }
+};
+
+function generatePolicy(principalId: string, effect: "Allow" | "Deny", resource: string): APIGatewayAuthorizerResult {
+  return {
+    principalId: principalId,
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "execute-api:Invoke",
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
+    },
+  };
+}
