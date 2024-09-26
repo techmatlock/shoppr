b76a26161450b035f83c997e1c6e2ebcf670b494
Mark Matlock <techmatlock@gmail.com>
2024-09-26 10:14:18 -0700

    Added auth provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,24 +3,37 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import RootLayout from "./layouts/root-layout";
 import { SignInPage } from "./pages/sign-in";
 import { SignUpPage } from "./pages/sign-up";
 import { NotFoundPage } from "./pages/not-found";
 import { IndexPage } from "./pages";
 import { ItemsProvider } from "./context/ItemsContext";
 import { UserProvider } from "./context/UserContext";
+import { ProtectedRoutes } from "./pages/protected-routes";
+import { AuthProvider } from "./context/AuthContext";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
-    element: <RootLayout />,
+    path: "/sign-in",
+    element: <SignInPage />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUpPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+  {
+    element: <ProtectedRoutes />,
     children: [
-      { path: "/", element: <IndexPage /> },
-      { path: "/sign-in/*", element: <SignInPage /> },
-      { path: "/sign-up/*", element: <SignUpPage /> },
-      { path: "*", element: <NotFoundPage /> },
+      {
+        path: "/",
+        element: <IndexPage />,
+      },
     ],
   },
 ]);
@@ -28,11 +41,13 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <ItemsProvider>
-          <RouterProvider router={router} />
-        </ItemsProvider>
-      </UserProvider>
+      <AuthProvider>
+        <UserProvider>
+          <ItemsProvider>
+            <RouterProvider router={router} />
+          </ItemsProvider>
+        </UserProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
