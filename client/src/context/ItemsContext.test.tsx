e5a8c2e25d2f7b31149de4970c39821b3ad99c64
Mark Matlock <techmatlock@gmail.com>
2024-09-27 12:16:59 -0700

    Removed ItemsContext.test.tsx

--- client/src/context/ItemsContext.test.tsx
+++
@@ -1,8 +0,0 @@
-import { useItems } from "./useItems";
-const { checkIfNeedExists } = useItems();
-
-describe("checkIfNeedExists", () => {
-  test("A userId and shoppingItemId should return true", () => {
-    expect(checkIfNeedExists(2, 1)).toBe(true);
-  });
-});
