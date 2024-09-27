9220a924898a6b85e77198569d0b739be114c946
Mark Matlock <techmatlock@gmail.com>
2024-09-27 12:18:13 -0700

    Added save to localStorage test

--- client/src/lib/data.test.tsx
+++ client/src/lib/data.test.tsx
@@ -1,7 +1,22 @@
 import { getInitials } from "./data";
+import { saveToLocalStorage } from "./localStorageUtil";
 
 describe("Get initials", () => {
   test("Mark Daniels should result in MD", () => {
     expect(getInitials("Mark Daniels")).toBe("MD");
+  });
+});
+
+describe("LocalStorage Util", () => {
+  beforeEach(() => {
+    Storage.prototype.setItem = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Should save a value to localStorage", () => {
+    saveToLocalStorage("um.auth", "{user: {userId: 1, username: mdaniels}, token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9}");
   });
 });
