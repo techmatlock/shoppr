68213600a1cd1bb748b58299b58a39d924b9d821
Mark Matlock <techmatlock@gmail.com>
2025-03-03 15:51:08 -0800

    Fixed protected route to not stay on sign-up route

--- client/src/pages/protected-routes.tsx
+++ client/src/pages/protected-routes.tsx
@@ -1,8 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
 
 export function ProtectedRoutes() {
   const { isAuthenticated, loading } = useAuth();
+
+  useEffect(() => {}, [isAuthenticated]); // Checks if isAuthenticated changes after sign-in POST request
 
   if (loading) {
     return <div>Loading...</div>;
